cffdd60d8d687a3bcf05c2403d64499e1900e158
Ines Pucker <ines.pucker@gmx.de>
2017-02-06 15:16:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much."
+const autobots = [
+  { name: 'Optimus Prime', strength: 5, isTransformed: false, },
+  { name: 'Ironhide', strength: 3, isTransformed: false, },
+  { name: 'Bumblebee', strength: 2.5, isTransformed: false, },
+  { name: 'Ratchet', strength: 1.5, isTransformed: false, },
+];
+
+const transformedAutobotsWithMap = autobots.map(function (autobot) {
+  return Object.assign({}, autobot, { name: "SoCute",
+    strength: autobot.strength * 4,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobotsWithMap);
